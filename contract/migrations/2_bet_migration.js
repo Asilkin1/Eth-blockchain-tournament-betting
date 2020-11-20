2334b917ee4e11f87272385b30109bb1100efdea
Alexander Silkin <mrmajestickk@gmail.com>
2020-11-20 15:19:30 -0500

    Migrating without parameters

--- contract/migrations/2_bet_migration.js
+++ contract/migrations/2_bet_migration.js
@@ -1,17 +1,6 @@
 const BetContract = artifacts.require('Bet');
 
-const fs = require('fs');
-
-// Data from tournament json file
-const DATA = fs.readFileSync('../../eth-bet/src/TOURNAMENT_DATA/PAST_t.json');
-
-// Parameters passed to constructor
-const _minBet= 200;
-const _name="Hello World Contract"
-const max_players = 4;
-const _answer = 'A'
-
 // Deploy a contract with parameters
 module.exports = function(deployer){
-    deployer.deploy(BetContract,_minBet,_name, max_players, _answer);
+    deployer.deploy(BetContract);
 }
