70d7df11b4121a0134fb1160df83c1f3dfa1c4c1
Alexander Silkin <mrmajestickk@gmail.com>
2020-11-20 22:24:17 -0500

    Added options A and B for betting

--- eth-bet/src/components/single_tournament/singletournament.js
+++ eth-bet/src/components/single_tournament/singletournament.js
@@ -1,25 +1,31 @@
-import { Toast, ListGroup, Badge, ListGroupItem } from 'react-bootstrap';
+import { Row,Col,ListGroup, Badge, Button } from 'react-bootstrap';
+
 
 // Pass item data inside the component
 // This is stateless component the data is passed from the parent
-function SingleTournament({ itemData }) {
+function SingleTournament({ itemData, participateInTournament }) {
+
+    function onClick(e){
+        console.log("Answer selected " + e.target.value + "key: ");
+        // Send choice back to the contract with minimumVlaue
+        participateInTournament(e.target.value,itemData.minBet);
+    }
 
     return (
 
         <ListGroup.Item>
-         
+
             <h6>{itemData.title}</h6>
             <ListGroup horizontal>
-        
-                    <p>Type: <Badge variant='light'>{itemData.type}</Badge></p>
-                    <p>Players: <Badge variant='primary'>{itemData.players}</Badge></p>
-             
-                    <p>Condition: <Badge variant='light'>{itemData.condition}</Badge></p>
-                    <p>Bank:<Badge variant='warning'>{itemData.bank}</Badge></p>
-               
-                    <p>Creator:<Badge variant='danger'>{itemData.creator}</Badge></p>
-                    <p>Rating:<Badge variant='info'>{itemData.rating}</Badge></p>
-               
+
+                <p>Players: <Badge variant='primary'>{itemData.players}</Badge></p>
+
+                <p>Bank:<Badge variant='warning'>{itemData.bank}</Badge></p>
+
+                <Col><Button onClick={onClick} value={itemData.A}>{itemData.A}</Button></Col>
+
+                <Col><Button onClick={onClick} value={itemData.B}>{itemData.B}</Button></Col>
+
             </ListGroup>
 
 
