b80e2a7017ade6c5dc5bc8481dd625bd087a453b
Alexander Silkin <mrmajestickk@gmail.com>
2020-11-20 22:23:50 -0500

    Changed betting mechanism
    Can bet on each tournament from the list

--- eth-bet/src/components/tournament_list/tournamenlist.js
+++ eth-bet/src/components/tournament_list/tournamenlist.js
@@ -3,11 +3,11 @@ import SingleTournament from '../single_tournament/singletournament';
 
 // This component defines how the list of tournaments will looks like
 // Receives props from App.js
-function TournamentList({ tournaments }) {
+function TournamentList({ tournaments, participateInTournament }) {
     // Pass props further down to each child component
     const currentTournaments = tournaments.map((e) => {
         // Unique key should be passed for each component in the list
-        return <SingleTournament itemData={e} key={e.key}></SingleTournament>
+        return <SingleTournament itemData={e} key={e.key} participateInTournament={participateInTournament}></SingleTournament>
     });
     return (
         <ListGroup>
