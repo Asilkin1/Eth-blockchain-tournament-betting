c9fd5120228cc7018b025f37e5a8eb5a775ed08e
Alexander Silkin <mrmajestickk@gmail.com>
2020-12-03 22:17:23 -0500

    Update App.js
    - Pass current player address to Nav component
    - Pass block number to navbar
    - Add event listener for past events
    - Add async function to call contract methods

--- eth-bet/src/App.js
+++ eth-bet/src/App.js
@@ -1,26 +1,24 @@
 import { Container, Row, Col, ModalFooter } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-import Tx from 'ethereumjs-tx';
 // ------------------------------------------------------------------App components imports
 import Navigation from './components/navigation/navigation';
 import TournamenList from './components/tournament_list/tournamenlist';
 import WinnersList from './components/winnerslist/winnerslist';
 // ----------------------------------------------------------------------------------------
-import {web3, betContract, NETWORK_TYPE} from './config'; // Backend imports
+import {web3, betContract, NETWORK_TYPE, player1, player2,betAddress} from './config'; // Backend imports
 // ----------------------------------------------------------------------------------------
-
 import './App.css';
+
+const Tx = require('ethereumjs-tx').Transaction;
+
 
 function App() {
   
-  // Set players wallets by index in the wallet-provider
-  const [player1, setPlayerOne] = useState('');
-  const [player1Private, setPlayerOnePrivate] = useState('');
-  const [player2, setPlayerTwo] = useState('');
-  const [player2Private, setPlayerTwoPrivate] = useState('');
-
   // Set first player as an avtive. Then switch back to the second
   const [ActivePlayerOne, setActivePlayer] = useState(true);
+
+  // Block number
+  const [ blockNumber, setBlockNumber] = useState('');
 
   const currentPlayer = ActivePlayerOne ? player1 : player2;
   
@@ -40,22 +38,24 @@ function App() {
    * 
    */
 
+
    // SEND ANSWER TO BACKEND ------------------------------------------------------------------------------------------------------------------------------------
 async function participateInTourney(answer){
-  web3.eth.getTransactionCount(currentPlayer, (err, txCount) => {
-    
+  web3.eth.getTransactionCount(currentPlayer.address, (err, txCount) => {
+      
     // Transaction object
     const txObject = {
       nonce: web3.utils.toHex(txCount),
-      gasLimit: web3.utils.toHex(468000), // Raise the gas limit to a much higher amount
+      gasLimit: web3.utils.toHex(4680000), // Raise the gas limit to a much higher amount
       gasPrice: web3.utils.toHex(web3.utils.toWei('10', 'wei')),
-      to: betContract,
+      to: betAddress,
+      value:web3.utils.toHex(web3.utils.toWei('5', 'wei')),
       data: betContract.methods.participateInTourney(answer).encodeABI()
     }
 
     // Which network
     const tx = NETWORK_TYPE === 'private' ? new Tx(txObject) : new Tx(txObject, { 'chain': 'ropsten' });
-    tx.sign(Buffer.from(currentPlayer == player1 ? player1Private.substr(2) : player2Private.substr(2), 'hex'))   // Suppose to be a private key
+    tx.sign(Buffer.from(currentPlayer == player1 ? player1.privateKey.substr(2) : player2.privateKey.substr(2), 'hex'))   // Suppose to be a private key
 
     const serializedTx = tx.serialize()
     const raw = '0x' + serializedTx.toString('hex')
@@ -65,14 +65,35 @@ async function participateInTourney(answer){
       if (!err) {
         console.log();
       }
+
+      else{
+        console.log(err);
+      }
     })
   })
+}
+
+async function getCurrentTournamentIndex(){
+  let currentTournamentIndex = await betContract.methods.getCurrentTournamentIndex().call().then(console.log);
+  return currentTournamentIndex;
+}
+// Get a list of movies
+async function getTournamentsAsync(currentTournament) {
+  
+  let tournament = await betContract.methods.getCurrentTournament(currentTournament).call();
+  let tournamentsLoaded = [];
+  for (let m of tournament) {
+
+    tournamentsLoaded.push({key: '0xDA8F4421' + Math.random().toString().split('.').join('r'), minBet:m.minBet,title:m.name,players:m.maxPlayers,correct:m.correctResult,A:m.A,B:m.B});
+  }
+
+  setTournaments(tournamentsLoaded);     // Set state of the movie
 }
   
 // THIS FUNCTION IS RESPOSIBLE FOR ACTUALLY BETTING ON THE TOURNAMENT -----------------------------------------------------------------------------------
   function participateInTournament(answer) {
 
-    //participateInTourney(answer);     // This function will actually send an answer to the contract
+    participateInTourney(answer);     // This function will actually send an answer to the contract
 
     // Check switching mechanism between current and next player
     console.log(currentPlayer);
@@ -85,29 +106,33 @@ async function participateInTourney(answer){
     }
   }
 
-  // Connecting to backend
+ async function getLenOfT(){
+  let howManyTournaments =  await betContract.methods.getTournamentsCount().call();
+  return howManyTournaments;
+ }
+
   // Test call to functions here
   useEffect(() =>{
-          // Try connect to web3
-          try{
-            // Set account one and two
-            web3.eth.getAccounts().then( account => {
-              setPlayerOne(account[0]);
-              setPlayerTwo(account[1]);
-            });
-            // Get account from web3
-            console.log(web3.eth.getAccounts());
-           
-    
-          } catch(error){
-            alert(`Failed to load web3, accounts, or contract.Check console for details`);
-            console.log(error);
-    }
 
-     // print accounts
-     console.log(player1);
-     console.log(player2);
-  },[])
+    // Get past events
+    betContract.getPastEvents('AtLeastOneWinner','NoWinners', {
+      filter: {}, // Using an array means OR: e.g. 20 or 23
+      fromBlock: 0,
+      toBlock: 'latest'
+  }, function(error, events){ console.log(events); })
+  .then(function(events){
+      console.log(events) // same results as the optional callback above
+  });
+
+    // Get block number
+    web3.eth.getBlockNumber((err,res) =>{
+      if(!err){
+        setBlockNumber(res);
+      } else{
+        console.log(err);
+      }
+    });
+  },[blockNumber])
 
   // Dummy data for winners. Should be received from contract
   const winners = [{ key: 'sdfsdf', address: '0x00a6d02Aa0dF9EBE9FBE62F683eea474de0D3CFa', bank: '$100' },
@@ -118,7 +143,7 @@ async function participateInTourney(answer){
 
       {/* Top level content */}
       <header className="App-header">
-        <Navigation />
+        <Navigation currentPlayer = {currentPlayer.address} blockNumber={blockNumber} />
 
       </header>
 
@@ -153,12 +178,10 @@ async function participateInTourney(answer){
 
         </Row>
         {/* Footer */}
-        <ModalFooter>
+        <ModalFooter fixed="bottom">
           <Col>ETH-BET 2020 @All Rights Reserved </Col>
         </ModalFooter>
       </Container>
-
-
     </div>
   );
 }
