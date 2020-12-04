0e63749da879bda1ee87d113959d10cef945675b
Alexander Silkin <mrmajestickk@gmail.com>
2020-12-03 22:41:18 -0500

    New tournament cart design

--- eth-bet/src/components/single_tournament/singletournament.js
+++ eth-bet/src/components/single_tournament/singletournament.js
@@ -1,4 +1,4 @@
-import { Row, Col, ListGroup, Badge, Button } from 'react-bootstrap';
+import { Jumbotron, Col, Row, ListGroup, Badge, Button } from 'react-bootstrap';
 
 // Pass item data inside the component
 // This is stateless component the data is passed from the parent
@@ -12,27 +12,29 @@ function SingleTournament({ itemData, participateInTournament }) {
     }
 
     return (
-        <ListGroup.Item>
 
-            <h6>{itemData.title}</h6>
-            <ListGroup horizontal>
+        <Jumbotron>
+            <h1>{itemData.title}</h1>
+            <Col>
+            <h2><Badge variant='primary'></Badge></h2></Col>
+            <Button disabled block variant="info">
+                Maximum participants <Badge variant="light">{itemData.players}</Badge> 
+            </Button>
 
-                <p>Players: <Badge variant='primary'>{itemData.players}</Badge></p>
-
-                <p>Minimum bet:<Badge variant='warning'>{itemData.minbet}</Badge></p>
-
+            <Button disabled block variant="info">
+                Minimum bet <Badge variant="light">{itemData.minbet}</Badge> 
+            </Button>
+            
+            <Row md="12">
                 {/* Choice A */}
-                <Col><Button onClick={onClick} value={itemData.A}>{itemData.A}</Button></Col>
+                <Col><Button block size="lg" variant="warning" onClick={onClick} value={itemData.A}>{itemData.A}</Button></Col>
 
                 {/* Choice B */}
-                <Col><Button onClick={onClick} value={itemData.B}>{itemData.B}</Button></Col>
-
-                
-
-            </ListGroup>
+                <Col><Button block size="lg" variant="danger" onClick={onClick} value={itemData.B}>{itemData.B}</Button></Col>
+            </Row>
 
             <p>Tournament:{itemData.key}</p>
-        </ListGroup.Item>
+        </Jumbotron>
 
     )
 }
